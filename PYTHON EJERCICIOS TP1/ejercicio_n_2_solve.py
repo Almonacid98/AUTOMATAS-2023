d45fd5fde0daf9913b53d818b0ff8d5337a9e613
Almonacid98 <gabriel98almonacid@hotmail.com>
2023-03-27 18:52:07 -0300

    Ejercicio 2 en camino a resolverse, ya funciona con las primeras operaciones del tp

--- PYTHON EJERCICIOS TP1/ejercicio_n_2_solve.py
+++ PYTHON EJERCICIOS TP1/ejercicio_n_2_solve.py
@@ -2,25 +2,36 @@ def solve(operation):
     numbers = []
     operators = []
     current_number = ""
-    for char in operation:
-        if char.isdigit():
-            current_number += char
+    out = []
+    sum = 0
+    for x in operation:
+        if x.isdigit():
+            current_number += x
         else:
             numbers.append(int(current_number))
             current_number = ""
-            operators.append(char)
+            operators.append(x)
     numbers.append(int(current_number))
     result = numbers[0]
     print(numbers)
     print(operators)
     for i in range(1, len(numbers)):
-            if operators[i-1] == "*":
-                result *= numbers[i]
-                print(result)
-            if operators[i-1] == "+":
-                result += numbers[i]
-                print(result)
-    return print(result)
+        if operators[i-1] == "*":
+            result = numbers[i-1] * numbers[i]
+            numbers.pop(i)
+            numbers.pop(i-1)
+    operators.remove("*")
+    out.append(result)
+
+    for i in range(1, len(numbers)):
+        if operators[i-1] == "+":
+            sum = numbers[i-1] + numbers[i] + out[0]
+
+    print(out)
+ 
+    print(numbers)
+    print(operators)
+    return sum
                         
-operation = input("Ingrese una operacion con sumas y multiplicacion: ")
-solve(operation)
+operation = input("Enter an operation with addition and multiplication: ")
+print(solve(operation))
