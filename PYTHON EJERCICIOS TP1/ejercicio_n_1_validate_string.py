a36bef5d9cdb342e2b1306f19a3339839cd6b975
Almonacid98 <gabriel98almonacid@hotmail.com>
2023-03-27 16:11:36 -0300

    Avance Ejercicio 1

--- PYTHON EJERCICIOS TP1/ejercicio_n_1_validate_string.py
+++ PYTHON EJERCICIOS TP1/ejercicio_n_1_validate_string.py
@@ -1,2 +1,13 @@
-def validate_string():
-    pass
+def validate_string(string):
+    conditions = [
+        any(char.isalnum() for char in string),
+        any(char.isalpha() for char in string),
+        any(char.isupper() for char in string),
+        any(char.islower() for char in string),
+        any(char.isdigit() for char in string),
+        len(string) >= 8
+    ]
+    return print(conditions)
+
+argument = input("Enter an argument: ")
+validate_string(argument)
