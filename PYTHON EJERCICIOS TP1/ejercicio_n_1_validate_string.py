20c6f8c5badd1616dad617ae907cbc323d0338a3
D3XTRO12 <mirazopablo@gmail.com>
2023-03-27 16:26:28 -0300

    Version final EJ1

--- PYTHON EJERCICIOS TP1/ejercicio_n_1_validate_string.py
+++ PYTHON EJERCICIOS TP1/ejercicio_n_1_validate_string.py
@@ -1,13 +1,21 @@
-def validate_string(string):
-    conditions = [
-        any(char.isalnum() for char in string),
-        any(char.isalpha() for char in string),
-        any(char.isupper() for char in string),
-        any(char.islower() for char in string),
-        any(char.isdigit() for char in string),
-        len(string) >= 8
-    ]
-    return print(conditions)
-
-argument = input("Enter an argument: ")
-validate_string(argument)
+def validate_string(str_in):
+    str_long = 0
+    long_output = False    
+    val_alnum = any(x.isalnum() for x in str_in)
+    val_alpha = any(x.isalpha() for x in str_in)
+    val_upper = any(x.isupper() for x in str_in)
+    val_low = any(x.islower() for x in str_in)
+    val_digit = any(x.isdigit() for x in str_in)
+    str_long = len(str_in)
+    if str_long >= 8:
+        long_output = True
+    else:
+        long_output = False
+    print(val_alnum)
+    print(val_alpha)
+    print(val_upper)
+    print(val_low)
+    print(val_digit)
+    print(long_output)
+str_argument = input("Enter an argument: ")
+validate_string(str_argument)
