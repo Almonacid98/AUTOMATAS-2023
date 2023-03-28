aa97ca6aa645f59bdf3bc0bb452c7a1c703b869c
D3XTRO12 <mirazopablo@gmail.com>
2023-03-27 23:03:09 -0300

    Version final EJ2

--- PYTHON EJERCICIOS TP1/ejercicio_n_2_solve.py
+++ PYTHON EJERCICIOS TP1/ejercicio_n_2_solve.py
@@ -1,50 +1,25 @@
+def multiplication(multiplicator):
+    mul = multiplicator
+    l_mul = mul.split("*")
+    l_mul = [int(x) for x in l_mul]
+    mul_result = 1
+    for element in l_mul:
+        mul_result *= element
+    return mul_result
 def solve(operation):
-    numbers = []
-    operators = []
-    current_number = ""
-    out = []
-    sum = 0
-    for x in operation:
-        if x.isdigit():
-            current_number += x
+    operation = operation.replace(" ","")
+    op_list = operation.split("+")
+    mult = 0
+    for i in range(0, len(op_list)):
+        elemnt = op_list[i]
+        if "*" in elemnt:
+            mult = elemnt
+            result = multiplication(mult)
+            op_list[i] = result
         else:
-            numbers.append(int(current_number))
-            current_number = ""
-            operators.append(x)
-    numbers.append(int(current_number))
-    result = numbers[0]
-    print(numbers)
-    print(operators)
-    for i in range(1, len(numbers)):
-        if operators[i-1] == "*":
-            result = numbers[i-1] * numbers[i]
-            numbers.pop(i)
-            numbers.pop(i-1)
-    operators.remove("*")
-    out.append(result)
-
-    for i in range(1, len(numbers)):
-        if operators[i-1] == "+":
-            sum = numbers[i-1] + numbers[i] + out[0]
-
-    print(out)
- 
-    print(numbers)
-    print(operators)
-    return sum
-                        
-operation = input("Enter an operation with addition and multiplication: ")
-print(solve(operation))
-
+            pass
+    op_list = [int(x) for x in op_list]
+    final_result = sum(op_list)
+    print("The final result is: ", final_result)
 cad = str(input("..."))
-cad = cad.replace(" ","")
-lista = cad.split("+")
-mult = []
-print(lista)
-for i in range(0, len(lista)):
-    elemnt = lista[i]
-    if "*" in elemnt:
-        mult = elemnt
-        print(mult)
-    break
-"""def multiplicator()"""
+solve(cad)
