4d035c1b5d2dc3f1b3fb8bff9f30635a8c60ba92
Almonacid98 <gabriel98almonacid@hotmail.com>
2023-04-22 20:53:02 -0300

    orden

--- PYTHON EJERCICIOS TP2/ejercicio_n_2_afn/ejercicio_1_2.py
+++ PYTHON EJERCICIOS TP2/ejercicio_n_2_afn/ejercicio_1_2.py
@@ -7,7 +7,7 @@ class Afn:
     def read_in(self, input_string):
         return bool(re.match(self.pattern, input_string))
 
-pattern = r'^(a|b)*$'
+pattern = r'^(aa|b)*(a|bb)*$'
 in_str = str(input("Enter the corresponding character: "))
 automation = Afn(pattern)
 print(automation.read_in(in_str))
