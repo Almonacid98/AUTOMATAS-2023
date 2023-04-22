6952c4d3128c86397ad74ccd7efa98131607347f
D3XTRO12 <mirazopablo@gmail.com>
2023-04-22 16:08:03 -0300

    AFD 1.1 y 1.2 listos

---
+++ EJ1.2 - AFD.py
@@ -0,0 +1,13 @@
+import re
+
+class AFD:
+    def __init__(self, pattern):
+        self.pattern = pattern
+        
+    def read_in(self, input_string):
+        return bool(re.match(self.pattern, input_string))
+
+pattern = r'^(aa|b)*(a|bb)*$'
+in_str = str(input("..."))
+automation = AFD(pattern)
+print(automation.read_in(in_str))
