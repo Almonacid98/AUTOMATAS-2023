db4123d18964cd26ddfca0373974f1140565b04b
Almonacid98 <gabriel98almonacid@hotmail.com>
2023-04-24 16:21:14 -0300

    Ejercicios en clase terminados

--- PYTHON EJERCICIOS TP2/ejercicio_n_1_afn/ejercicio_1_1.py
+++ PYTHON EJERCICIOS TP2/ejercicio_n_1_afn/ejercicio_1_1.py
@@ -1,13 +1,27 @@
-import re
-
 class Afn:
-    def __init__(self, pattern):
-        self.pattern = pattern
-        
-    def read_in(self, input_string):
-        return bool(re.match(self.pattern, input_string))
+    def __init__(self, transitions, initial_state, ok_states):
+        self.transitions = transitions
+        self.intial_state = initial_state
+        self.ok_states = set(ok_states)
+        "self.in_str = in_str"
+    
+    def read_in (self, in_str):
+        current = set([self.intial_state])
+        for char in in_str:
+            next_state = set()
+            for state in current:
+                if (state, char) in self.transitions:
+                    next_state |= self.transitions[(state, char)]
+                current = next_state
+        return bool(current & self.ok_states)
 
-pattern = r'^(a|b)*$'
+marks = {
+    (0, 'a'): {0,1},
+    (0, 'b'): {0,1},
+    (1, 'a'): {1},
+    (1, 'b'): {1},}
+start_state = 0
+ok_states = {0,1}
 in_str = str(input("Enter the corresponding character: "))
-automation = Afn(pattern)
+automation = Afn(marks, start_state, ok_states)
 print(automation.read_in(in_str))
--- PYTHON EJERCICIOS TP2/ejercicio_n_2_afn/ejercicio_1_2.py
+++ PYTHON EJERCICIOS TP2/ejercicio_n_2_afn/ejercicio_1_2.py
@@ -1,13 +1,31 @@
-import re
-
 class Afn:
-    def __init__(self, pattern):
-        self.pattern = pattern
+    def __init__(self, transition_function, init_state, ok_states):
+        self.transition_function = transition_function
+        self.init_state = init_state
+        self.ok_states = ok_states
         
-    def read_in(self, input_string):
-        return bool(re.match(self.pattern, input_string))
+    def read_in(self, in_str):
+        current_states = {self.init_state}
+        for char in in_str:
+            next_states = set()
+            for state in current_states:
+                next_states |= self.transition_function.get((state, char), set())
+            current_states = next_states
+        return any(state in self.ok_states for state in current_states)
 
-pattern = r'^(aa|b)*(a|bb)*$'
-in_str = str(input("Enter the corresponding character: "))
-automation = Afn(pattern)
-print(automation.read_in(in_str))
+transition_function = {
+    (0, 'a'): {1},
+    (0, 'b'): {0},
+    (1, 'a'): {2},
+    (1, 'b'): {0},
+    (2, 'a'): {2},
+    (2, 'b'): {3},
+    (3, 'a'): {3},
+    (3, 'b'): {3},
+}
+
+init_state = 0
+ok_states = {0, 2, 3}
+str_in = str(input("Enter the corresponding character: "))
+automaton = Afn(transition_function, init_state, ok_states)
+print(automaton.read_in(str_in))
