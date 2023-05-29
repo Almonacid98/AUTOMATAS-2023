58212191dd56d22c60919bfba3e51dc3586c3239
Almonacid98 <gabriel98almonacid@hotmail.com>
2023-05-29 19:22:01 -0300

    Arreglo del ejercicio de la url

--- TRABAJO PRACTICO N 1/ANALIZADOR LEXICO/b_url/url.py
+++ TRABAJO PRACTICO N 1/ANALIZADOR LEXICO/b_url/url.py
@@ -1,25 +1,16 @@
 import re
-
-url = re.compile("^(https?://)+(www\.)+([a-zA-Z0-9.-])+\.(com|es|ar|net|org|um\.edu\.ar|edu\.ar|edu\.com|edu\.org|cl|co|cn|ae|au|br|ca|nz)+\.(ar|cl|co|cn|ae|au|br|ca|nz|es)$")
+url = re.compile("^(https?://)+(www\.)+([a-zA-Z0-9.-])+\.(com|ar|net|org|edu\.ar|edu\.com|edu\.org)$")
+url_op2 = re.compile("^(www\.)+([a-zA-Z0-9.-])+\.(com|es|ar|net|org|um\.edu\.ar|edu\.ar|edu\.com|edu\.org|cl|co|cn|ae|au|br|ca|nz)+\.(ar|cl|co|cn|ae|au|br|ca|nz|es)$")
+url_op3 = re.compile("^([a-zA-Z0-9.-])+\.(com|es|co|ar|net|org|um|edu)+\.(ar|cl|co|cn|ae|au|br|ca|nz|es|edu)+\.(ar|cl|co|cn|ae|au|br|ca|nz|es|edu)$")
 
 print("VALIDACION DE URL, PUNTO B")
 agregarurl = [input("INGRESE LA URL: " u'')]
 
 for pagina in agregarurl:
-    match = url.search(pagina)
-    if match:
+    match_one = url.search(pagina)
+    match_two = url_op2.search(pagina)
+    match_tree = url_op3.search(pagina)
+    if match_one or match_two or match_tree:
         print('{:<30}  {}'.format(pagina, 'URL INGRESADA CORRECTAMENTE'))
-    else:
-        url = re.compile("^(www\.)+([a-zA-Z0-9.-])+\.(com|es|ar|net|org|um\.edu\.ar|edu\.ar|edu\.com|edu\.org|cl|co|cn|ae|au|br|ca|nz)+\.(ar|cl|co|cn|ae|au|br|ca|nz|es)$")
-        for pagina in agregarurl:
-            match = url.search(pagina)
-            if match:
-                print('{:<30}  {}'.format(pagina, 'URL INGRESADA CORRECTAMENTE'))
-            else:
-                url = re.compile("^([a-zA-Z0-9.-])+\.(com|es|co|ar|net|org|edu\.ar|um\.edu\.ar|edu\.com|edu\.org|cl|co|cn|ae|au|br|ca|nz)+\.(ar|cl|co|cn|ae|au|br|ca|nz|es)$")
-                for pagina in agregarurl:
-                    match = url.search(pagina)
-                    if match:
-                        print('{:<30}  {}'.format(pagina, 'URL INGRESADA CORRECTAMENTE'))
-                    else:
-                        print('{:<30} {}'.format(pagina, 'URL INVALIDA'))
+    else: 
+        print('{:<30}  {}'.format(pagina, 'URL INVALIDA'))
