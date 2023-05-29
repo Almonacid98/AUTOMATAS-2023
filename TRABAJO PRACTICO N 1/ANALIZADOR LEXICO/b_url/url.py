3469fd253fc8eb9cec8395a14fa01bd02973bf33
Almonacid98 <gabriel98almonacid@hotmail.com>
2023-05-29 18:26:19 -0300

    Arreglo del url

--- TRABAJO PRACTICO N 1/ANALIZADOR LEXICO/b_url/url.py
+++ TRABAJO PRACTICO N 1/ANALIZADOR LEXICO/b_url/url.py
@@ -1,9 +1,25 @@
 import re
-url = re.compile("^(https?://)+(www\.)+([a-zA-Z0-9.-])+\.(com|ar|net|org|edu\.ar|edu\.com|edu\.org)$")
+
+url = re.compile("^(https?://)+(www\.)+([a-zA-Z0-9.-])+\.(com|es|ar|net|org|um\.edu\.ar|edu\.ar|edu\.com|edu\.org|cl|co|cn|ae|au|br|ca|nz)+\.(ar|cl|co|cn|ae|au|br|ca|nz|es)$")
 
 print("VALIDACION DE URL, PUNTO B")
 agregarurl = [input("INGRESE LA URL: " u'')]
 
 for pagina in agregarurl:
     match = url.search(pagina)
-    print('{:<30}  {}'.format(pagina, 'URL INGRESADA CORRECTAMENTE' if match else 'URL INVALIDA'))
+    if match:
+        print('{:<30}  {}'.format(pagina, 'URL INGRESADA CORRECTAMENTE'))
+    else:
+        url = re.compile("^(www\.)+([a-zA-Z0-9.-])+\.(com|es|ar|net|org|um\.edu\.ar|edu\.ar|edu\.com|edu\.org|cl|co|cn|ae|au|br|ca|nz)+\.(ar|cl|co|cn|ae|au|br|ca|nz|es)$")
+        for pagina in agregarurl:
+            match = url.search(pagina)
+            if match:
+                print('{:<30}  {}'.format(pagina, 'URL INGRESADA CORRECTAMENTE'))
+            else:
+                url = re.compile("^([a-zA-Z0-9.-])+\.(com|es|co|ar|net|org|edu\.ar|um\.edu\.ar|edu\.com|edu\.org|cl|co|cn|ae|au|br|ca|nz)+\.(ar|cl|co|cn|ae|au|br|ca|nz|es)$")
+                for pagina in agregarurl:
+                    match = url.search(pagina)
+                    if match:
+                        print('{:<30}  {}'.format(pagina, 'URL INGRESADA CORRECTAMENTE'))
+                    else:
+                        print('{:<30} {}'.format(pagina, 'URL INVALIDA'))
